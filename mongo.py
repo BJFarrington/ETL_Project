906be0a76d0c1add9348e6662218cc3f5326abbe
B Farrington <BWayne422@yahoo.com>
2019-05-15 19:06:00 -0500

    Fixed weatherapi data.ipynb

--- mongo.py
+++ mongo.py
@@ -7,6 +7,8 @@ client = pymongo.MongoClient(conn)
 
 db = client.idaho_potatoes_db
 
-potatoes_col = db.temp
 
-potatoes_col.insert_one(all_data)
+
+#potatoes_col = db.temp
+
+#potatoes_col.insert_one(all_data)
